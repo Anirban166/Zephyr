0cb6660db8c3f86232c65d30034d0819e8d5557c
Joseph Vargovich <46722295+DovahCraft@users.noreply.github.com>
2022-04-03 18:03:52 -0700

    Job and Node classes done, timestamps.
    Co-authored-by: Ani/Raven <bloodraven166@gmail.com>

--- batch_schedule_starter.cpp
+++ batch_schedule_starter.cpp
@@ -1,4 +1,63 @@
 #include "batch_schedule_starter.h"
+// Compile and execute using:
+// g++ batch_schedule_starter.cpp -o batch_schedule_starter && ./batch_schedule_starter 
+/**
+   Args: Number of jobs, CPUs available, Memory Available, Nodes available,
+   Scheduling algorithm, Verbose
+ **/ 
+int main(int argc, char* argv[]) 
+{
+    if(argc != 2)
+    {
+        std::cout << "Please enter the following arguments:\n";  
+        std::cout << "The number of jobs to schedule.\n";  
+        exit(-1); 
+    }    
+    std::cout << "The arguments you entered are:\n";
+    for(int i = 0; i < argc; ++i) 
+        std::cout << "Argument " << i << ": " << argv[i] << "\n";  
 
+    int jobCount = atoi(argv[1]);
+    int nodeCount = 3;
+    int coresPerNode = 8;
+    //100 GiB per node, i.e. 102400 MiB
+    int memoryPerNode = 102400;
+    // Generate jobs
+    std::vector<Job> jobList = buildPresetJobs();
+    // Choose algorithm to run
+
+}
+
+std::vector<Job> buildPresetJobs()
+{   
+    std::vector<Job> jobList;
+    //auto ct = steady_clock::now(); // ::time_point // std::chrono::seconds -> timestamp
+   // steady_clock currTime = duration_cast<std::chrono::seconds>(steady_clock::now());
+   time_point<system_clock, seconds> start = time_point_cast<seconds>(system_clock::now());
+   std::time_t now_c = std::chrono::system_clock::to_time_t(start);
+   // Job firstJob = Job(1, currTime, currTime + offset)
+   //  std::cout << start_duration;
+   
+
+  std::cout << now_c << "\n";
+
+   return jobList;
+}
+/*
+std::vector<Job> buildRandomizedJobs(int jobCount, std::vector<Job> jobList)
+{
+    
+
+    
+  //  double random = lowLimit + (rand() % higherLimit - 1)
+    for(int i = 0; i < jobCount; i++)  
+    {
+
+        Job currentJob = Job(jobCount, startTime, waitTime, requestedRunTime, trueRunTime, stopTime,
+                        //    requestedCPUs, requestedMemory, usedMemory, jobStatus, userId, groupId, precedingJobId, thinkTimeFromPreceding);
+        jobList.push_back(currentJob);
+
+    }
+} */
 
 
--- batch_schedule_starter.h
+++ batch_schedule_starter.h
@@ -1,4 +1,6 @@
+#include <ctime>
 #include <cmath>
+#include <random>
 #include <chrono>
 #include <vector>
 #include <cstdlib>
@@ -14,7 +16,9 @@ class Job
 {
     public:
       int jobNum;
-      int startTime;
+      //Time the job was submitted by the user
+      timestamp submitTime;
+      //Time the job actually starts running on CPU(s) 
       timestamp startTime;
       timestamp waitTime;
       timestamp requestedRunTime;
@@ -30,7 +34,59 @@ class Job
       int groupId;
       int precedingJobId;
       timestamp thinkTimeFromPreceding;
+
+      Job(
+        int jobNum,
+        timestamp startTime,
+        timestamp waitTime,
+        timestamp requestedRunTime,
+        timestamp trueRunTime,
+        timestamp stopTime,
+        double requestedCPUs,
+        std::vector<timestamp> cpuTimes,
+        double requestedMemory,
+        double usedMemory,
+        STATUS jobStatus,
+        int userId,
+        int groupId,
+        int precedingJobId,
+        timestamp thinkTimeFromPreceding){
+          this->jobNum = jobNum;
+          this->startTime = startTime;
+          this->waitTime = waitTime;
+          this->requestedRunTime = requestedRunTime;
+          this->trueRunTime = trueRunTime;
+          this->stopTime = stopTime;
+          this->requestedCPUs = requestedCPUs;
+          //Actual time run on each CPU. CPU 1 == trueRunTime. multiple CPUs (parallel) - take all the time measurements, average it
+          this->cpuTimes = cpuTimes; 
+          this->requestedMemory = requestedMemory;
+          this->usedMemory = usedMemory;
+          this->jobStatus = jobStatus;
+          this->userId = userId;
+          this->groupId = groupId;
+          this->precedingJobId = precedingJobId;
+          this->thinkTimeFromPreceding = thinkTimeFromPreceding;
+      }
 };
+
+
+class Node {
+  int coreCount;
+  int coresUsed;
+  int memoryAmount;
+  int memoryUsed;
+
+  Node(int coreCount, int coresUsed, int memoryAmount, int memoryUsed){
+    this->coreCount = coreCount;
+    this->coresUsed = coresUsed;
+    this->memoryAmount = memoryAmount;
+    this->memoryUsed = memoryUsed;
+  }
+
+};
+
+std::vector<Job> buildPresetJobs();
 
 /*
 auto startTime = high_resolution_clock::now();
